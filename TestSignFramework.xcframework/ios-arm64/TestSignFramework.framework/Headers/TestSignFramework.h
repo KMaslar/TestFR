7dbd4fa8fac3ff70c76eafd51cea32eee7274a73
Kostadina Geceska <kostadinag@nextsense.com>
2024-06-28 15:03:44 +0200

    initial commit

---
+++ TestSignFramework.xcframework/ios-arm64/TestSignFramework.framework/Headers/TestSignFramework.h
@@ -0,0 +1,18 @@
+//
+//  TestSignFramework.h
+//  TestSignFramework
+//
+//  Created by Kostadina Gecevska on 28.6.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestSignFramework.
+FOUNDATION_EXPORT double TestSignFrameworkVersionNumber;
+
+//! Project version string for TestSignFramework.
+FOUNDATION_EXPORT const unsigned char TestSignFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestSignFramework/PublicHeader.h>
+
+
